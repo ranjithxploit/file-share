4642189d1fa002f8f7fe7f1ec940e0fd6c238b23
Ranjith <cyberexpertranjith@gmail.com>
2025-09-18 22:55:55 +0530

    second commit

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-#! /usr/bin/env python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/success/yes')
+def success(name):
+    return "welcome to %s!" %name
+
+@app.route('/login' , methods = ['GET','POST'])
+def login():
+    
